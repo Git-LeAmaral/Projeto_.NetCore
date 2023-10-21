4d964cef3f974143efa22859869523855951dc33
Leandro Amaral <lramaral02m@gmail.com>
2023-10-21 07:14:53 -0300

    Aplicando os atributos Data Annotations

--- LehLanches/Models/Categoria.cs
+++ LehLanches/Models/Categoria.cs
@@ -1,9 +1,22 @@
-﻿namespace LehLanches.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LehLanches.Models
 {
+    [Table("Categorias")]
     public class Categoria
     {
+        [Key]
         public int CategoriaId { get; set; }
+
+        [Display(Name = "Nome")]
+        [Required(ErrorMessage = "Informe o nome da categoria")]
+        [StringLength(100,ErrorMessage = "O tamanho máximo é 100 caracteres")]
         public string CategoriaNome { get; set; }
+
+        [Display(Name = "Descrição")]
+        [Required(ErrorMessage = "Informe a descrição da categoria")]
+        [StringLength(200, ErrorMessage = "O tamanho máximo é 200 caracteres")]
         public string Descricao { get; set; }
 
         public List<Lanche> Lanches { get; set;}
